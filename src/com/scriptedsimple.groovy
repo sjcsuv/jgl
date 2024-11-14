1c3ce584c0a6f002583fa4342f488206268bf991
sjcsuv <103010888+sjcsuv@users.noreply.github.com>
2024-11-14 10:49:44 +0530

    Update scriptedsimple.groovy

--- src/com/scriptedsimple.groovy
+++ src/com/scriptedsimple.groovy
@@ -7,13 +7,27 @@ node {
         stage('Create File') {
             echo 'Creating a sample file...'
             // This creates a file named "sample.txt" with some text
-            sh 'echo "Hello, Jenkins Pipeline!" > samplee.txt'
+             steps {
+                echo 'Creating a sample file...'
+                script {
+                    if (isUnix()) {
+                         sh 'echo "Hello, Jenkins Pipeline!" > samplee.txt'
+                    } else {
+                        bat sh 'echo "Hello, Jenkins Pipeline!" > samplee.txt'
+                    }
+           
         }
         
         stage('Read File') {
             echo 'Reading the contents of the file...'
             // This displays the contents of the created file
-            sh 'cat samplee.txt'
+            script {
+                    if (isUnix()) {
+                        sh 'cat sample.txt'
+                    } else {
+                        bat 'type sample.txt'
+                    }
+            }
         }
     } finally {
         echo 'Pipeline completed.'
